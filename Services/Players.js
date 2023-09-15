278acd14cce38f449b4ca78507fcad1e1eb70b9e
jlawthom <jacklawthom@yahoo.co.uk>
2023-09-15 13:57:09 +0100

    Updated scores

--- Services/Players.js
+++ Services/Players.js
@@ -11,10 +11,10 @@ function Player(playerId, name, team, pool, tieBreak, points, pin){
 }
 
 const PLAYERS = [
-    Player(1, "Gramma", "New Zealand", "A",-1, 0, 1000),
-    Player(2, "Claire", "France", "A", 0, 4, 1234),
+    Player(1, "Gramma", "New Zealand", "A",0, 0, 1000),
+    Player(2, "Claire", "France", "A", 0, 8, 1234),
     Player(3, "Ci & Jens", "Italy", "A", 0, 5, 1010),
-    Player(4, "Naomi", "Uruguay", "A", 0, 0, 2000),
+    Player(4, "Naomi", "Uruguay", "A", -1, 0, 2000),
     Player(5, "Chrissy", "Namibia", "A", -2, 0, 9999),
     Player(6, "Ian", "South Africa", "B", 0, 4, 2222),
     Player(7, "Martine", "Ireland", "B", 0, 5, 2345),
